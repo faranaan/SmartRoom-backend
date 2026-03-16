f603c08aec3444d88f26216f2467ab87096185e2
faranaan <aan21102006@gmail.com>
2026-03-16 21:29:51 +0700

    chore: allow vercel domain in CORS policy

--- SmartRoom.API/Program.cs
+++ SmartRoom.API/Program.cs
@@ -70,7 +70,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+            policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "https://smart-room-frontend.vercel.app")
             .AllowAnyHeader()
             .AllowAnyMethod();
         });
